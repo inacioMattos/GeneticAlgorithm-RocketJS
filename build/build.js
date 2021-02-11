7a464ecd6471b128412525909fbf8a7f7242a119
Inácio Mattos <inaciotm@hotmail.com>
2021-02-11 19:55:47 -0300

    Initial commit

--- build/build.js
+++ build/build.js
@@ -43,38 +43,13 @@ var ColorHelper = (function () {
     };
     return ColorHelper;
 }());
-var numberOfShapes = 15;
-var speed;
 function setup() {
     console.log("🚀 - Setup initialized - P5 is running");
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER).noFill().frameRate(30);
-    speed = createSlider(0, 15, 3, 1);
-    speed.position(10, 10);
-    speed.style("width", "80px");
 }
 function draw() {
     background(0);
-    translate(width / 2, height / 2);
-    var colorsArr = ColorHelper.getColorsArray(numberOfShapes);
-    var baseSpeed = (frameCount / 500) * speed.value();
-    for (var i = 0; i < numberOfShapes; i++) {
-        var npoints = 3 + i;
-        var radius = 20 * i;
-        var angle = TWO_PI / npoints;
-        var spin = baseSpeed * (numberOfShapes - i);
-        strokeWeight(3 + i).stroke(colorsArr[i]);
-        push();
-        rotate(spin);
-        beginShape();
-        for (var a = 0; a < TWO_PI; a += angle) {
-            var sx = cos(a) * radius;
-            var sy = sin(a) * radius;
-            vertex(sx, sy);
-        }
-        endShape(CLOSE);
-        pop();
-    }
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
